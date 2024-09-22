d9e9441e929f026fc52e996b216e390154a35108
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-09-22 15:56:54 +0200

    Odd error

--- VBS/main.py
+++ VBS/main.py
@@ -1,27 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_sqlalchemy import SQLAlchemy
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
-
-
 all_books = []
 
+
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books-collection.db"
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"<Book {self.title}> "
+
+with app.app_context():
+    db.create_all()
+
+with app.app_context():
+    new_book = Book(id=1, title="Database Design, Implementation and Management", author="Steven Morris, Carlos Coronel, Keeley", rating=4)
+    db.session.add(new_book)
+    db.session.commit()
 
 @app.route('/')
 def home():
     global all_books
+
     return render_template(template_name_or_list='index.html', all_books=all_books)
 
 
